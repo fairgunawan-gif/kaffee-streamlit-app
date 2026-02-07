cefba951832b4f38af7cb976a6d51ae0fd3847cf
fairgunawan-gif <fairgunawan@googlemail.com>
2026-02-07 18:52:05 +0100

    Describe your change

--- kaffee.py
+++ kaffee.py
@@ -18,6 +18,7 @@ df["Verzoegerung_Min"] = (
 
 # keep only the three routes you want (adapt names if yours are just "A", "B", "C")
 routes = ["Route_A", "Route_B", "Route_C"]
+
 df_plot = (
     df[df["Lieferroute"].isin(routes)]
     .groupby(["Kalenderwoche", "Lieferroute"], as_index=False)["Umsatzverlust_EUR"]
@@ -30,72 +31,44 @@ df_plot_verz = (
     .mean()
 )
 
+
+max_kw = df_plot["Kalenderwoche"].max()
+last_4_weeks = list(range(max_kw - 3, max_kw + 1))
+
+df_plot_last4 = df_plot[df_plot["Kalenderwoche"].isin(last_4_weeks)]
+df_plot_verz_last4 = df_plot_verz[df_plot_verz["Kalenderwoche"].isin(last_4_weeks)]
+
+
 st.subheader("Mittelwert von Umsatzverlust pro Kalenderwoche")
-# pivot = df_plot.pivot(
-#     index="Kalenderwoche",
-#     columns="Lieferroute",
-#     values="Umsatzverlust_EUR",
-# ).sort_index()
-
-# st.line_chart(pivot)
-
 
 chart_umsatz = (
-    alt.Chart(df_plot)
+    alt.Chart(df_plot_last4)
     .mark_line()
     .encode(
-        x=alt.X(
-            "Kalenderwoche:O",
-            title="Kalenderwoche"
-        ),
-        y=alt.Y(
-            "Umsatzverlust_EUR:Q",
-            title="Umsatzverlust (Euro)"
-        ),
-        color=alt.Color(
-            "Lieferroute:N",
-            title="Lieferroute"
-        )
+        x=alt.X("Kalenderwoche:O", title="Kalenderwoche"),
+        y=alt.Y("Umsatzverlust_EUR:Q", title="Umsatzverlust (Euro)"),
+        color=alt.Color("Lieferroute:N", title="Lieferroute"),
     )
 )
 
 st.altair_chart(chart_umsatz, use_container_width=True)
 
+
 st.subheader("Mittelwert von Verzoegerung pro Kalenderwoche")
-# pivot_verz = df_plot_verz.pivot(
-#     index="Kalenderwoche",
-#     columns="Lieferroute",
-#     values="Verzoegerung_Min",
-# ).sort_index()
-
-# st.line_chart(pivot_verz)
-
 
 chart_verz = (
-    alt.Chart(df_plot_verz)
+    alt.Chart(df_plot_verz_last4)
     .mark_line()
     .encode(
-        x=alt.X(
-            "Kalenderwoche:O",
-            title="Kalenderwoche"
-        ),
-        y=alt.Y(
-            "Verzoegerung_Min:Q",
-            title="Verzoegerung (Minuten)"
-        ),
-        color=alt.Color(
-            "Lieferroute:N",
-            title="Lieferroute"
-        )
+        x=alt.X("Kalenderwoche:O", title="Kalenderwoche"),
+        y=alt.Y("Verzoegerung_Min:Q", title="Verzögerung (Minuten)"),
+        color=alt.Color("Lieferroute:N", title="Lieferroute"),
     )
 )
 
 st.altair_chart(chart_verz, use_container_width=True)
 
-
 st.markdown("Last updated 07.02.26", text_alignment="left")
-
-
 
 # Whenever you change the code:
 # In VS Code:
